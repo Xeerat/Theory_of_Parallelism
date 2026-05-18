5d352d208a93fec210a2bd1ac1d62dc3e165b283
Xeerat <yuzhaninov_amz@mail.ru>
2026-05-18 22:53:10 +0700

    Fix

--- task_6/laplace2d.h
+++ task_6/laplace2d.h
@@ -2,6 +2,6 @@ void initialize(double *__restrict A, double *__restrict Anew, int m, int n);
 
 double calcNext(double *__restrict A, double *__restrict Anew, int m, int n);
         
-void swap(double *__restrict A, double *__restrict Anew, int m, int n);
+void swap(double *__restrict A, double *__restrict Anew);
 
 void deallocate(double *__restrict A, double *__restrict Anew);
